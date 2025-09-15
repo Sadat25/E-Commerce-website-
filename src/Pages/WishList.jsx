2a2088793bacd7cf672f419c7e104c459f614504
Sadat25 <you@example.com>
2025-09-15 22:23:25 +0600

    home

--- src/Pages/WishList.jsx
+++ src/Pages/WishList.jsx
@@ -12,12 +12,17 @@ const WishList = () => {
           <h3 className='font-Poppins text-xl leading-6.5'>Wishlist (4)</h3>
           <Btn1 btn={"Move All To Bag"} />
         </div>
-        <div></div>
-        <div className='flex items-center justify-between'>
-          <SecHeading heading={"Just For You"} headingClassName="text-black" />
+        <div>slick slider</div>
+        <div className='flex items-center justify-between mt-20 mb-15'>
+            <div className='flex items-center gap-4'>
+              <div className=' bg-black w-5 h-10 relative'>
+                <div className='w-5 h-10 bg-primary rounded-md absolute top-0 left-0'></div>
+              </div>
+              <h2 className="font-Poppins text-black leading-6.5 lg:text-[20px] ">Just For You</h2>
+            </div>
           <Btn1 btn={"See All"} />
         </div>
-        <div></div>
+        <div>slick slider</div>
       </Container>
       <ToastContainer
         position="top-right"
